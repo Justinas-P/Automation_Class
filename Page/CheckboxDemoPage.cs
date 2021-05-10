1f7bc6871d42a54dbaa4c2be88c4598d52aa4711
DESKTOP-T9QA6CU\Justas <j.petrauskas22@gmail.com>
2021-05-10 11:21:32 +0300

    Added HomeWork_3 class

---
+++ Page/CheckboxDemoPage.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_3.Page
+{
+    public class CheckboxDemoPage
+    {
+        private static IWebDriver _driver;
+
+        private IWebElement singleCheckBoxDemoSelect => _driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement singleCheckBoxDemoResult => _driver.FindElement(By.Id("txtAge"));
+        private IWebElement multipleCheckBox_1 => _driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(3) > label > input"));
+        private IWebElement multipleCheckBox_2 => _driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(4) > label > input"));
+        private IWebElement multipleCheckBox_3 => _driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(5) > label > input"));
+        private IWebElement multipleCheckBox_4 => _driver.FindElement(By.CssSelector("#easycont > div > div.col-md-6.text-left > div:nth-child(5) > div.panel-body > div:nth-child(6) > label > input"));
+        private IWebElement multipleCheckBoxDemoButton => _driver.FindElement(By.Id("check1"));
+
+        public CheckboxDemoPage(IWebDriver webDriver) 
+        {
+            _driver = webDriver;
+        }
+
+        public void CheckSingleCheckbox()
+        {
+            singleCheckBoxDemoSelect.Click();
+        }
+        public void VerifySingleCheckBoxDemoResult(string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, singleCheckBoxDemoResult.Text, "CheckBox is not selected");
+        }
+        public void CheckAllMultipleCheckBoxOptions()
+        {
+            multipleCheckBox_1.Click();
+            multipleCheckBox_2.Click();
+            multipleCheckBox_3.Click();
+            multipleCheckBox_4.Click();
+        }
+        public void VerifyMultipleCheckBoxDemoButtonValue(string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, multipleCheckBoxDemoButton.Text, "Not all checkboxes selected");
+        }
+    }
+}
---
+++ Test/CheckboxDemoTest.cs
@@ -0,0 +1,55 @@
+﻿using HomeWork_3.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_3.Test
+{
+    public class CheckboxDemoTest
+    {
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize(); 
+            _driver.Url = "https://www.seleniumeasy.com/test/basic-checkbox-demo.html";
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); 
+            /*WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(x => _driver.FindElement(By.Id("at-cv-lightbox-close")).Displayed); 
+            _driver.FindElement(By.Id("at-cv-lightbox-close")).Click(); 
+            */
+        }
+
+        [OneTimeTearDown] 
+        public static void TearDown()
+        {
+           // _driver.Close();
+        }
+
+        [TestCase("Success - Check box is checked", TestName = "SingleCheckBoxTest")]
+        
+
+        public static void TestOneCheckBox(string result)
+        {
+            CheckboxDemoPage page = new CheckboxDemoPage(_driver);
+            page.CheckSingleCheckbox();
+            page.VerifySingleCheckBoxDemoResult(result);
+        }
+
+        [TestCase("Uncheck All", TestName = "MultipleCheckBoxTest")]
+        public static void TestMultipleCheckBox(string result)
+        {
+            CheckboxDemoPage page = new CheckboxDemoPage(_driver);
+            page.CheckAllMultipleCheckBoxOptions();
+            page.VerifyMultipleCheckBoxDemoButtonValue(result);
+        }
+    }
+}
